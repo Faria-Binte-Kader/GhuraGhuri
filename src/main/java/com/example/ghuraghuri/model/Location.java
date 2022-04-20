62e0116222ab18142f820e736a43423c9478eadb
Faria-Binte-Kader <fariabintekader67@gmail.com>
2022-04-20 21:49:36 +0600

    location and pin merged

--- src/main/java/com/example/ghuraghuri/model/Location.java
+++ src/main/java/com/example/ghuraghuri/model/Location.java
@@ -8,25 +8,80 @@ public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Long id;
+    private int id;
+    private String username;
+    private String description;
+    private String rating;
+    private String lat;
+    private String lng;
     private String name;
     private String division;
-    private String district;
-    private String address;
     private String type;
-    private byte[] image;
     private String imageURL;
+
+    public Location(String username, String description, String rating, String lat, String lng, String name, String division, String type,  String imageURL) {
+        this.username = username;
+        this.description = description;
+        this.rating = rating;
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+        this.division = division;
+        this.type = type;
+        this.imageURL = imageURL;
+    }
+
 
     public Location() {
 
     }
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
     }
 
     public String getName() {
@@ -45,36 +100,12 @@ public class Location {
         this.division = division;
     }
 
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public String getType() {
         return type;
     }
 
     public void setType(String type) {
         this.type = type;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
-
-    public void setImage(byte[] image) {
-        this.image = image;
     }
 
     public String getImageURL() {
